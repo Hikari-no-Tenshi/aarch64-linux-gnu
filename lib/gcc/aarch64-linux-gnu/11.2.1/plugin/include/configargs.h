93376d47cb07084e0a816c96a84a03ac3d87d34e
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-08-20 21:14:41 +0300

    Initial commit of gcc 11

---
+++ lib/gcc/aarch64-linux-gnu/11.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror --with-pkgversion='HnT GCC' CFLAGS='-g0 -O3 -fstack-protector-strong -fuse-linker-plugin -ffunction-sections -fdata-sections' CXXFLAGS='-g0 -O3 -fstack-protector-strong -fuse-linker-plugin -ffunction-sections -fdata-sections' --enable-languages=c,c++ --target=aarch64-linux-gnu --prefix=/media/serhij/data2/builder/aarch64-linux-gnu";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
